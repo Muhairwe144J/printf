45f7e247aafb7d90d7cadc117b5bbe7a0188e5bd
Muhairwe144J <mnasasirashafik144j@gmail.com>
2023-07-26 01:36:43 +0300

    printf

--- _printf.c
+++ _printf.c
@@ -52,14 +52,16 @@ int flags, width, precision, size, buff_ind = 0;
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
+ * print_buffer - Function tha prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
+	{
 		write(1, &buffer[0], *buff_ind);
 
 	*buff_ind = 0;
+	}
 }
